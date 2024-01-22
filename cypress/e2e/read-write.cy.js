8642a20eba7c8036d7ecbe5465c945aeeb429f14
zimonturkish <simoturco85@gmail.com>
2024-01-22 09:54:29 +0100

    how to read and test a JSON file

--- cypress/e2e/read-write.cy.js
+++ cypress/e2e/read-write.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="Cypress"/>
 
+// how to write a JSON file
 describe('Read/Write into JSON file', () => {
   it('passes', () => {
     cy.writeFile('test-data/data.json', {name: 'Peter', email: 'peter@email.de'})
+    cy.readFile('test-data/data.json').then((user) => {
+    expect(user.name).to.equal('Peter'),
+    expect(user.email).to.equal('peter@email.de')
   })
+})
 })
