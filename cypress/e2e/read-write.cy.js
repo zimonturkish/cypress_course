96ffe33d3637075e48361295d072505cf4ab4f0e
zimonturkish <simoturco85@gmail.com>
2024-01-22 10:04:31 +0100

    Generate a plain text file

--- cypress/e2e/read-write.cy.js
+++ cypress/e2e/read-write.cy.js
@@ -1,12 +1,20 @@
 /// <reference types="Cypress"/>
 
-// how to write a JSON file
-describe('Read/Write into JSON file', () => {
-  it('passes', () => {
+describe('Cypress Files', () => {
+  // how to write a JSON file
+  it('Read/Write into JSON file', () => {
     cy.writeFile('test-data/data.json', {name: 'Peter', email: 'peter@email.de'})
+
+    // how to read and test a JSON file
     cy.readFile('test-data/data.json').then((user) => {
     expect(user.name).to.equal('Peter'),
     expect(user.email).to.equal('peter@email.de')
   })
-})
+  })
+
+  //how to write a plain text file
+  it('Write into plain text file', () => {
+    cy.writeFile('test-data/plain.txt', 'Hello World', 'ascii')
+  })
+
 })
