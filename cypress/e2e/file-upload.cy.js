b3bd1748e5ce92c02c34247e2ded6ebd0be15e6a
zimonturkish <simoturco85@gmail.com>
2024-01-23 17:11:04 +0100

    How to upload a file

---
+++ cypress/e2e/file-upload.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress"/>
+
+describe('Cypress Files Upload', () => {
+  it('Should upload JSON example file', () => {
+    cy.visit('https://the-internet.herokuapp.com/upload')
+    cy.get('#file-upload').selectFile('cypress/fixtures/example.json')
+    cy.get('#file-submit').click()
+    cy.get('h3').should('be.visible','File uploaded!')
+  })
+})
