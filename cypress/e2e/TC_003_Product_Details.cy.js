68a37e18343f9c851ca13844fca1346581ca7d60
zimonturkish <simoturco85@gmail.com>
2024-01-24 16:41:18 +0100

    Completing Test for product details

--- cypress/e2e/TC_003_Product_Details.cy.js
+++ cypress/e2e/TC_003_Product_Details.cy.js
@@ -1,7 +1,24 @@
 /// <reference types="Cypress"/>
 
-describe('Test Product Details', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('TC_003_Product_Details', () => {
+  it('Should verify the displayed product details', () => {
+    
+    // Load page
+    cy.visit('https://www.demoblaze.com/')
+    
+    // Verify product on the page and click on it
+    cy.get('.hrefch').contains('Nexus 6').click()
+
+    // Verify that the product page has been opened
+    cy.get('.product-deatil').should('be.visible')
+
+    // Verify name product
+    cy.get('h2.name').contains('Nexus 6').should('be.visible')
+
+    // Verify price product
+    cy.get('h3.price-container').should('contain', '$650') 
+    
+    // Verify button
+    cy.get('.btn-success').contains('Add to cart')
   })
 })
